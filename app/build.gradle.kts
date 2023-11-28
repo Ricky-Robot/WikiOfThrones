6bb89ed903057dff911921dbbbcad601c332b7ce
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2023-11-28 10:48:31 -0600

    Feature: Configuración de Firebase para autenticación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,6 +48,8 @@ dependencies {
 
     // Para gifs animados
     implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
+    // Para firebase auth
+    implementation("com.google.firebase:firebase-auth:22.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
